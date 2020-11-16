a64b6077d731133afcc85a78294c50a54e8be901
RafaelRNG <rafael.gomila@hotmail.com>
2020-11-16 18:23:31 -0300

    criando metodos para criar, alterar, deletar e retornar todos de Review

--- src/main/java/com/rng/articles/controllers/ReviewController.java
+++ src/main/java/com/rng/articles/controllers/ReviewController.java
@@ -4,11 +4,12 @@ import com.rng.articles.entities.Review;
 import com.rng.articles.services.ReviewService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/reviews")
@@ -17,8 +18,43 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
+    @GetMapping
+    public ResponseEntity<List<Review>> findAll(){
+        return ResponseEntity.ok(reviewService.findAll());
+    }
+
+    @GetMapping(path = "/page")
+    public ResponseEntity<Page<Review>> pagination(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(name = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(name = "orderBy", defaultValue = "reviewTitle") String orderBy){
+
+        Page<Review> reviewPage = reviewService.pagination(page, linesPerPage, direction, orderBy);
+
+        return ResponseEntity.ok(reviewPage);
+    }
+
     @GetMapping(path = "/{id}")
     public ResponseEntity<Review> findById(@PathVariable Long id){
         return ResponseEntity.ok(reviewService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Review review){
+        reviewService.save(review);
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(review.getId()).toUri()).build();
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Review review){
+        reviewService.update(id, review);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        reviewService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/rng/articles/services/ReviewService.java
+++ src/main/java/com/rng/articles/services/ReviewService.java
@@ -4,7 +4,12 @@ import com.rng.articles.entities.Review;
 import com.rng.articles.repositories.ReviewRepository;
 import com.rng.articles.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReviewService {
@@ -12,7 +17,32 @@ public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
 
+    public List<Review> findAll(){
+        return reviewRepository.findAll();
+    }
+
+    public Page<Review> pagination(Integer page, Integer linesPerPage, String direction, String orderBy){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+        return reviewRepository.findAll(pageRequest);
+    }
+
     public Review findById(Long id){
         return reviewRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found, ID: " + id));
+    }
+
+    public void save(Review review){
+        reviewRepository.save(review);
+    }
+
+    public Review update(Long id, Review review){
+        Review reviewUpdate = reviewRepository.findById(id).get();
+        review.setId(reviewUpdate.getId());
+        reviewRepository.save(review);
+        return review;
+    }
+
+    public void deleteById(Long id){
+        reviewRepository.deleteById(id);
     }
 }
