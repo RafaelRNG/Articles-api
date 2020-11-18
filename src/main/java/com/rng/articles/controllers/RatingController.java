7349c78f3202e6163029a09f8442214f73e9313a
RafaelRNG <rafael.gomila@hotmail.com>
2020-11-18 03:10:09 -0300

    criando metodos para criar, alterar, deletar e retornar todos de Rating

--- src/main/java/com/rng/articles/controllers/RatingController.java
+++ src/main/java/com/rng/articles/controllers/RatingController.java
@@ -3,11 +3,12 @@ package com.rng.articles.controllers;
 import com.rng.articles.entities.Rating;
 import com.rng.articles.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/ratings")
@@ -16,8 +17,44 @@ public class RatingController {
     @Autowired
     private RatingService ratingService;
 
+    @GetMapping
+    public ResponseEntity<List<Rating>> findAll(){
+        return ResponseEntity.ok(ratingService.findAll());
+    }
+
+    @GetMapping(path = "/page")
+    public ResponseEntity<Page<Rating>> pagination(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(name = "direction", defaultValue = "ASC") String diretion,
+            @RequestParam(name = "orderBy", defaultValue = "id") String orderBy){
+
+        return ResponseEntity.ok(ratingService.pagination(page, linesPerPage, diretion, orderBy));
+    }
+
     @GetMapping(path = "/{id}")
     public ResponseEntity<Rating> findById(@PathVariable Long id){
         return ResponseEntity.ok(ratingService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Rating rating){
+        ratingService.save(rating);
+
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(rating.getId()).toUri()).build();
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Rating> update(Long id, Rating rating){
+        ratingService.update(id, rating);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        ratingService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/rng/articles/services/RatingService.java
+++ src/main/java/com/rng/articles/services/RatingService.java
@@ -4,7 +4,12 @@ import com.rng.articles.entities.Rating;
 import com.rng.articles.repositories.RatingRepository;
 import com.rng.articles.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RatingService {
@@ -12,7 +17,34 @@ public class RatingService {
     @Autowired
     private RatingRepository ratingRepository;
 
+    public List<Rating> findAll(){
+        return ratingRepository.findAll();
+    }
+
+    public Page<Rating> pagination(Integer page, Integer linesPerPage, String direction, String orderBy){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+        return ratingRepository.findAll(pageRequest);
+    }
+
     public Rating findById(Long id){
         return ratingRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found, ID: " + id));
+    }
+
+    public void save(Rating rating){
+        ratingRepository.save(rating);
+    }
+
+    public Rating update(Long id, Rating rating){
+        Rating ratingUpdate = ratingRepository.findById(id).get();
+        rating.setId(ratingUpdate.getId());
+
+        ratingRepository.save(rating);
+        return rating;
+    }
+
+
+    public void deleteById(Long id){
+        ratingRepository.deleteById(id);
     }
 }
