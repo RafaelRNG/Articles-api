98b6689ddc74bfaf000ad159fdd3a690b64862ea
RafaelRNG <rafael.gomila@hotmail.com>
2020-11-18 02:13:51 -0300

    criando metodos para criar, alterar, deletar e retornar todos de Article

--- src/main/java/com/rng/articles/controllers/ArticleController.java
+++ src/main/java/com/rng/articles/controllers/ArticleController.java
@@ -3,11 +3,12 @@ package com.rng.articles.controllers;
 import com.rng.articles.entities.Article;
 import com.rng.articles.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/articles")
@@ -16,8 +17,45 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
+    @GetMapping
+    public ResponseEntity<List<Article>> findAll(){
+        return ResponseEntity.ok(articleService.findAll());
+    }
+
+    @GetMapping(path = "/page")
+    public ResponseEntity<Page<Article>> pagination(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(name = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(name = "orderBy", defaultValue = "title") String orderBy){
+
+         return ResponseEntity.ok(articleService.pagination(page, linesPerPage, direction, orderBy));
+    }
+
     @GetMapping(path = "/{id}")
     public ResponseEntity<Article> findById(@PathVariable Long id){
         return ResponseEntity.ok(articleService.findById(id));
     }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Article article){
+        articleService.save(article);
+
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().buildAndExpand(article.getId()).toUri()).build();
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody Article article){
+        articleService.update(id, article);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        articleService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/rng/articles/services/ArticleService.java
+++ src/main/java/com/rng/articles/services/ArticleService.java
@@ -4,7 +4,12 @@ import com.rng.articles.entities.Article;
 import com.rng.articles.repositories.ArticleRepository;
 import com.rng.articles.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ArticleService {
@@ -12,7 +17,33 @@ public class ArticleService {
     @Autowired
     private ArticleRepository articleRepository;
 
+    public List<Article> findAll(){
+        return articleRepository.findAll();
+    }
+
+    public Page<Article> pagination(Integer page, Integer linesPerPage, String direction, String orderBy){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+        return articleRepository.findAll(pageRequest);
+    }
+
     public Article findById(Long id){
         return articleRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found, ID: " + id));
+    }
+
+    public void save(Article article){
+        articleRepository.save(article);
+    }
+
+    public Article update(Long id, Article article){
+        Article articleUpdate = articleRepository.findById(id).get();
+        article.setId(articleUpdate.getId());
+        articleRepository.save(article);
+
+        return article;
+    }
+
+    public void deleteById(Long id){
+        articleRepository.deleteById(id);
     }
 }
